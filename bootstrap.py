6170f14f71a9cfeaaa5cb87bcf87c4527598354e
OfilioHA <herdocia20@gmail.com>
2025-04-16 14:14:11 -0600

    feat: :sparkles: Using variebles from .env

--- bootstrap.py
+++ bootstrap.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
 
 def create_app():
+   
+    load_dotenv()
+
     app = Flask(__name__)
+    
+    app.config['DEBUG'] = eval(os.getenv("MODE_DEBUG"))
+    
     return app
