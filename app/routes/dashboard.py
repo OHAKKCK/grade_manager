1d92242c1b7277d7b63bf3ad6d1d328208eda187
OfilioHA <herdocia20@gmail.com>
2025-05-20 15:48:57 -0600

    feat: :sparkles: Rendering App dashboar page

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 dashboard_bp = Blueprint('dashboard', __name__, url_prefix="/dashboard")
 
 
 @dashboard_bp.route('/', methods=['GET'])
-def dashboard():
-    return "Welcome to the Dashboard!"
+def index():
+    return render_template('dashboard.html')
