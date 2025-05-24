31c350044693aaadd301c6b24c8403f72df17ca1
OfilioHA <herdocia20@gmail.com>
2025-05-24 08:08:03 -0600

    feat: :sparkles: Create testing admin user

--- app/extensions/login.py
+++ app/extensions/login.py
@@ -11,5 +11,17 @@ def load_user(user_id):
 
 
 def extension_login(app: Flask):
+    from app.extensions.alchemy import alchemy
+    from app.models import User
+
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
+
+    with app.app_context():
+        alchemy.create_all()
+
+        if not User.query.filter_by(username='admin').first():
+            admin_user = User(username='admin', email='test@example.com')
+            admin_user.set_password('password')
+            alchemy.session.add(admin_user)
+            alchemy.session.commit()
